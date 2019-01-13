ac9b3821e525f6bdeb6909ced31c88cc4fa1b954
Pablo Rocha <jose.rocha3@gmail.com>
2019-01-13 10:04:58 -0600

    consistent grammar

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -103,7 +103,7 @@ const Contact = () => (
     />
     <Wrapper>
       <Name>{'Let\'s Collaborate'}</Name>
-      <Description>I am available for contract work or to simply talk tech.</Description>
+      <Description>I am available for contract work or to simply talk tech</Description>
       <Form action="https://formspree.io/pablo@dotdotdata.com" method="POST">
         <input type="text" name="_gotcha" style={{ display: 'none' }} />
         <input type="hidden" name="_subject" value="New submission from pablorocha.me" />
